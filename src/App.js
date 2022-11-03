7d9517cea7195a96614ac8f542715f79bd501e5f
Maralasai <r170773@rguktrkv.ac.in>
2022-11-03 15:36:46 +0530

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
 
 const App=()=> {
+  const[data,setData]=useState([])
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(
+      response=>setData(response.data)
+    )
+  })
   return (
     <div>
-    hello 
-      
+      {data.map(item=> <li key={item.id}>{item.title}</li>)}
     </div>
   )
 }
